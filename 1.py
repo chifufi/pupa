f7f8a9cd415ffc589dae16e47a1e33622803564d
chifufi <adginv26@yandex.ru>
2024-02-22 02:45:57 +0300

    1.0

--- 1.py
+++ 1.py
@@ -7,6 +7,9 @@ def show_text_window():
     window = tk.Toplevel(root)
     window.title("Вывод текста")
     
+    # Установка размера нового окна
+    window.geometry("300x100")
+    
     # Создание метки для вывода текста
     label = tk.Label(window, text=text)
     label.pack(padx=20, pady=20)
@@ -14,7 +17,7 @@ def show_text_window():
 # Создание основного окна
 root = tk.Tk()
 root.title("Главное окно")
-
+root.geometry("200x100")
 # Создание кнопки
 button = tk.Button(root, text="Показать текст", command=show_text_window)
 button.pack(padx=50, pady=20)
